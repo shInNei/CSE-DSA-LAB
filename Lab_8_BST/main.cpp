d63d31a2ba1b3924b60a3df4e5b1a88307a1c5a7
Thịnh Nguyễn <109053741+shInNei@users.noreply.github.com>
2023-11-10 17:37:43 +0700

    Add files via upload

---
+++ Lab_8_BST/main.cpp
@@ -0,0 +1,6 @@
+int rangeCount(BTNode* root, int lo, int hi) {
+    if(!root) return 0;
+    if(root->val >= lo && root->val <= hi) return 1 + rangeCount(root->left, lo, hi) + rangeCount(root->right, lo, hi);
+    else if (root->val < lo) return rangeCount(root->right, lo, hi);
+    else return rangeCount(root->left, lo, hi);
+}
